1ad6cabf6405eac6c8ec8f7e413dca9f65804a18
Manish Mani <manishpamu@hotmail.com>
2025-01-26 00:23:40 +0530

    Create 1_Print.pl

---
+++ 1_Print.pl
@@ -0,0 +1,17 @@
+#To execute any perl command in normal terminal use the below command> 
+> perl -e ' print "Hey \n" '
+
+
+#Command_1 : print 
+print "Hey \n"                        ; #Output: Hey
+
+
+#how to write a comment
+#! /bin/perl
+use strict;
+use warnings;
+use diagnostics;
+print "Hey \n";        #this is a comment
+
+
+
