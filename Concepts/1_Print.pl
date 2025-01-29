c60021a49fbaa961b3d7bd03ca255bb87d57033a
Manish Mani <manishpamu@hotmail.com>
2025-01-29 21:39:59 +0530

    Update 1_Print.pl

--- Concepts/1_Print.pl
+++ Concepts/1_Print.pl
@@ -1,13 +1,16 @@
+############################################################
 #Command_1 : print 
 print "Hey \n"         ; #Output: Hey
 
-
+############################################################
 #how to write a comment
+
 #! /bin/perl
 use strict;
 use warnings;
 use diagnostics;
+
 print "Hey \n";        #this is a comment
 
-
+############################################################
 
