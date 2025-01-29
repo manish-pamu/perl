19445135f4e276e35326987eb44de3dffd5699a1
Manish Mani <manishpamu@hotmail.com>
2025-01-30 00:37:53 +0530

    Create 2_my_part_a.pl

---
+++ Concepts/2_my_part_a.pl
@@ -0,0 +1,11 @@
+#SETTING UP A VARIABLE
+
+#single string value
+my $var = 'Manish';
+print "$var \n";                   #Output: Manish
+
+#multiple string values in an arry
+my @fishes = ('nemo','dory');
+print "@fishes\n";                 #Output: nemo dory
+print @fishes,"\n";                #Output: nemodory
+
