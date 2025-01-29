e404686332ca13de4555c507064373c42ff20735
Manish Mani <manishpamu@hotmail.com>
2025-01-30 00:58:05 +0530

    Update 2_my_part_a.pl

--- Concepts/2_my_part_a.pl
+++ Concepts/2_my_part_a.pl
@@ -1,4 +1,4 @@
-#SETTING UP A VARIABLE
+#SETTING UP VARIABLES USING 'my' COMMAND
 
 #single string value
 my $var = 'Manish';
@@ -23,3 +23,15 @@ my ( $name, $emp_id, $salary ) = ( 'Manish', 2187686, '10LPA' );
 print "Name          : $name" \n;                #Output: Name          : Manish
 print "Employee ID   : $Employee ID" \n;         #Output: Employee ID   : 2187686
 print "Salary        : $Salary" \n;              #Output: Salary        : 10LPA
+
+#Assign a variable dynamically into a sentence
+#when you are using single quotes ('') - whatever you see inside these quotes will get assigned to variable
+#when you are using double quotes ("") - the value substitution happens as mentioned below
+my $name = 'Manish';
+my $line = "$name is a bad boy";
+print "$line \n";                  #Output: Manish is a bad boy
+
+#incase of escape character
+my $time = "It\'s 6 O\'Clock";
+print "$time \n";                  #Output: It's 6 O'Clock
+
