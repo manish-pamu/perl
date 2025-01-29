9a082511b84c677d9bab91fe1a34fd6ff055799f
Manish Mani <manishpamu@hotmail.com>
2025-01-30 00:48:29 +0530

    Update 2_my_part_a.pl

--- Concepts/2_my_part_a.pl
+++ Concepts/2_my_part_a.pl
@@ -9,3 +9,17 @@ my @fishes = ('nemo','dory');
 print "@fishes\n";                 #Output: nemo dory
 print @fishes,"\n";                #Output: nemodory
 
+#store a variable's value into another new variable
+my @var1 = 'value';
+my @var2 = @var1;  
+print "@var2 \n";                  #Output: value
+
+#setting up an undefined variable
+my @undef_var;
+print "This is an undefined variable value - <@undef_var> \n";           #Output: This is an undefined variable value - <>
+
+#multi variable declaration in a single go
+my ( $name, $emp_id, $salary ) = ( 'Manish', 2187686, '10LPA' );
+print "Name          : $name" \n;                #Output: Name          : Manish
+print "Employee ID   : $Employee ID" \n;         #Output: Employee ID   : 2187686
+print "Salary        : $Salary" \n;              #Output: Salary        : 10LPA
