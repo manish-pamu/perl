9239fc7898504d76daa00945aaeec0042cca4d5a
Manish Mani <manishpamu@hotmail.com>
2025-01-30 17:39:51 +0530

    Update 2_my_part_a.pl

--- Concepts/2_my_part_a.pl
+++ Concepts/2_my_part_a.pl
@@ -1,8 +1,8 @@
 #SETTING UP VARIABLES USING 'my' COMMAND
 
 #single string value
-my $var = 'Manish';
-print "$var \n";                   #Output: Manish
+my $var = 'Value';
+print "$var \n";                   #Output: Value
 
 #multiple string values in an arry
 my @fishes = ('nemo','dory');
@@ -19,17 +19,17 @@ my @undef_var;
 print "This is an undefined variable value - <@undef_var> \n";           #Output: This is an undefined variable value - <>
 
 #multi variable declaration in a single go
-my ( $name, $emp_id, $salary ) = ( 'Manish', 2187686, '10LPA' );
-print "Name          : $name" \n;                #Output: Name          : Manish
-print "Employee ID   : $Employee ID" \n;         #Output: Employee ID   : 2187686
+my ( $name, $emp_id, $salary ) = ( 'Andrew', 123456, '10LPA' );
+print "Name          : $name" \n;                #Output: Name          : Andrew
+print "Employee ID   : $Employee ID" \n;         #Output: Employee ID   : 123456
 print "Salary        : $Salary" \n;              #Output: Salary        : 10LPA
 
 #Assign a variable dynamically into a sentence
 #when you are using single quotes ('') - whatever you see inside these quotes will get assigned to variable
 #when you are using double quotes ("") - the value substitution happens as mentioned below
-my $name = 'Manish';
-my $line = "$name is a bad boy";
-print "$line \n";                  #Output: Manish is a bad boy
+my $name = 'Andrew';
+my $line = "$name is from America";
+print "$line \n";                  #Output: Andrew is from America
 
 #incase of escape character
 my $time = "It\'s 6 O\'Clock";
