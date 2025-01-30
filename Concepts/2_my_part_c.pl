33da9b3d40299d2e435df61d48a4435cc1e5d51f
Manish Mani <manishpamu@hotmail.com>
2025-01-30 18:07:00 +0530

    Update 2_my_part_c.pl

--- Concepts/2_my_part_c.pl
+++ Concepts/2_my_part_c.pl
@@ -1,4 +1,9 @@
 #example_2
 
-my $line_1 = qq {Hello \"Hero_1\"} ;
-my $line_2
+my $line_1 = qq {Hello Hero_1} ;
+my $line_2 = qq <Hello Hero_2> ;
+my $line_3 = qq [Hello Hero_3] ;
+
+print "$line_1 \n";        #Output: Hello Hero_1
+print "$line_2 \n";        #Output: Hello Hero_2
+print "$line_3 \n";        #Output: Hello Hero_3
