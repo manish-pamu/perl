9bbe86ce9737170153179865cfbccdbd582b3956
Manish Mani <manishpamu@hotmail.com>
2025-01-30 18:11:28 +0530

    Create 2_my_part_d.pl

---
+++ Concepts/2_my_part_d.pl
@@ -0,0 +1,15 @@
+#using a string literal as head and tail... write a letter and store in in a variable
+
+my $var = <<"end_line";
+Dear Manager,
+This is an example code snippet.
+Thanks & Regards,
+Manish P
+end_line
+print "$var";
+
+#Output:
+Dear Manager,
+This is an example code snippet.
+Thanks & Regards,
+Manish P
