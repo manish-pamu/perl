323b9ade5ac79cdeed37be692036bb92877814e7
Manish Mani <manishpamu@hotmail.com>
2025-01-30 18:38:58 +0530

    Create 3_integer_float.pl

---
+++ Concepts/3_integer_float.pl
@@ -0,0 +1,9 @@
+#integer to float and vice versa
+
+print 2.89 * 10;		print "\n";	#Output: 28.9
+print int(2.89 * 10);		print "\n";	#Output: 28
+
+printf ( "%.3f", 10 );		print "\n";	#Output: 10.000
+
+print int( 10 / 3);		print "\n";	#Output: 3
+printf ( "%.6f", 10 / 3 );	print "\n";	#Output: 3.333333
